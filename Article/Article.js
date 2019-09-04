1fd739796ee8c5bdc1492c9c1b97c338673f34e4
WilliamBerlin76 <william.berlin76@gmail.com>
2019-09-04 15:23:43 -0500

    article.js complete

--- Article/Article.js
+++ Article/Article.js
@@ -85,6 +85,13 @@ const data = [
     thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
           Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
           Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
+  },
+  {
+    title: 'Is Bird Really the Word???',
+    date: `Sometime later than year 3000 AD after 'The Event'`,
+    firstParagraph: 'You',
+    secondParagraph: 'BETCHA',
+    thirdParagraph: 'Sonny'
   }
 ];
 
